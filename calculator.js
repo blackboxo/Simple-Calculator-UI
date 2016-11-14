c43267807bf5d11d64cf41c70afc82724a11a397
blackboxo <592276898@qq.com>
2016-11-14 13:19:07 +0800

    numeral and mod

--- calculator.js
+++ calculator.js
@@ -1,18 +1,44 @@
 var buttons = document.querySelectorAll('button');
-var operators = ['+','-','x','%'];
+var operators = ['+','-','x','%','M'];
+var systems = ['HEX','DEC','OCT','BIN'];
+var numSystem = 'DEC';
 var inputVal = '';
+var numVal = '';
 var isAnswer = 0;
 var tmp = 0;
 
 for(var i=0;i<buttons.length;i++){
   buttons[i].onclick=function(e) {
     var input = document.querySelector('#screen');
+    var input_HEX = document.querySelector('#HEX');
+    var input_DEC = document.querySelector('#DEC');
+    var input_OCT = document.querySelector('#OCT');
+    var input_BIN = document.querySelector('#BIN');
+    var onScreen_HEX = input_HEX.innerHTML;
+    var onScreen_DEC = input_DEC.innerHTML;
+    var onScreen_OCT = input_OCT.innerHTML;
+    var onScreen_BIN = input_BIN.innerHTML;
     var onScreen = input.innerHTML;
+
     var btnVal = this.innerHTML;
 
-    if(btnVal=='C'){
+    
+
+    if(btnVal=='Mod')
+    {
+      btnVal='M';
+    }
+    if(systems.indexOf(btnVal)>-1){
+        numSystem=btnVal;
+    }
+    else if(btnVal=='C'){
       input.innerHTML='';
       inputVal='';
+      input_HEX.innerHTML='';
+      input_DEC.innerHTML='';
+      input_OCT.innerHTML='';
+      input_BIN.innerHTML='';
+      numSystem='DEC';
       isAnswer=0;
     }
     else if (btnVal=='CE') {
@@ -23,6 +49,10 @@ for(var i=0;i<buttons.length;i++){
         inputVal=inputVal.substring(0,tmp+1);
       }
       input.innerHTML='';
+      input_HEX.innerHTML='';
+      input_DEC.innerHTML='';
+      input_OCT.innerHTML='';
+      input_BIN.innerHTML='';
     }
     else if (btnVal=='←') {
       onScreen=onScreen.substring(0,onScreen.length-1);
@@ -32,11 +62,38 @@ for(var i=0;i<buttons.length;i++){
     else if (btnVal=='=') {
       var equation = inputVal;
 
-      equation=equation.replace(/x/g,'*').replace(/%/g,'/');
+      if(numSystem=='HEX')
+        equation=equation.replace(/x/g,'*').replace(/%/g,'/').replace(/M/g,'%').replace(/[0-9A-Fa-f]+/g,function(number){return parseInt(number,16);});
+      else if(numSystem=='DEC')
+        equation=equation.replace(/x/g,'*').replace(/%/g,'/').replace(/M/g,'%').replace(/[0-9A-Fa-f]+/g,function(number){return parseInt(number,10);});
+      else if(numSystem=='OCT')
+        equation=equation.replace(/x/g,'*').replace(/%/g,'/').replace(/M/g,'%').replace(/[0-9A-Fa-f]+/g,function(number){return parseInt(number,8);});
+      else if(numSystem=='BIN')
+        equation=equation.replace(/x/g,'*').replace(/%/g,'/').replace(/M/g,'%').replace(/[0-9A-Fa-f]+/g,function(number){return parseInt(number,2);});
 
       if(equation){
-        input.innerHTML=eval(equation);
-        inputVal=eval(equation);
+
+        input_HEX.innerHTML=eval(equation).toString(16);
+        input_DEC.innerHTML=eval(equation).toString(10);
+        input_OCT.innerHTML=eval(equation).toString(8);
+        input_BIN.innerHTML=eval(equation).toString(2);
+
+        if(numSystem=='HEX'){
+          input.innerHTML=input_HEX.innerHTML;
+          inputVal=eval(equation).toString(16);}
+        else if(numSystem=='DEC'){
+          input.innerHTML=input_DEC.innerHTML;
+          inputVal=eval(equation).toString(10);
+        }
+        else if(numSystem=='OCT'){
+          input.innerHTML=input_OCT.innerHTML;
+          inputVal=eval(equation).toString(8);
+        }
+        else if(numSystem=='BIN'){
+          input.innerHTML=input_BIN.innerHTML;
+          inputVal=eval(equation).toString(2);
+        }
+
         isAnswer=1;
       }
 
@@ -69,6 +126,7 @@ for(var i=0;i<buttons.length;i++){
           isAnswer=0;
           tmp=0;
         }
+
       }
     }
   }
